d5fbe0702d83f66a309fa698d9c9833a5f707fa3
onwhim <36354709+onwhim@users.noreply.github.com>
2018-04-29 11:44:55 +0800

    Added the remoteip url parameter

--- Mc_recaptcha.php
+++ Mc_recaptcha.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mc_recaptcha {
 	protected $CI = NULL;
-	protected $secret_key = "6LejJlYAIZAlVFhH32MDY7-ma8fxm_5P"; // Set your key here and you're ready to go...
-	protected $site_key = "6LejJlYUABTB3AZchm0wCqZJu3rya46kyPA-"; // Set your site key here..
+	protected $secret_key = "6LejJlYUAAAAAIZAlVFhH32MDY7-ma8fx3HZm_5P"; // Set your key here and you're ready to go...
+	protected $site_key = "6LejJlYUAAAAABTB3AZchm0wCqZJu3rya46kyPA-"; // Set your key here and you're ready to go...
 
 	function __construct(){
 		$this->CI =& get_instance();
@@ -28,9 +28,10 @@ class Mc_recaptcha {
 	
 	public function validated(){
 		$grr_post = $this->CI->input->post('g-recaptcha-response');
-		$output = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$this->get_secret_key()."&response={$grr_post}");
+		$user_ip = $_SERVER['REMOTE_ADDR'];
+		$output = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$this->get_secret_key()."&response={$grr_post}&remoteip=".$user_ip);
 		$output = json_decode($output, TRUE);
-
+		
 		if ($output['success']=='1') {
 			return TRUE;
 		}
