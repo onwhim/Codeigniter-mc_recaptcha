63f7822dbffafb504535e4256393b68a9bac190b
onwhim <36354709+onwhim@users.noreply.github.com>
2018-04-29 12:34:20 +0800

    Custom form validation library

---
+++ MY_Form_validation.php
@@ -0,0 +1,23 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Form_validation extends CI_Form_validation {
+	protected $CI = NULL;
+
+	function __construct(){
+		parent::__construct();
+		$this->CI =& get_instance();
+    }
+	
+	public function recaptcha_valid($g_recaptcha_response) {
+		$this->CI->load->library('Mc_recaptcha');
+		
+		if ($this->CI->mc_recaptcha->validated() == TRUE) {
+				return TRUE;
+		}
+
+		return FALSE;
+    }
+}
+
+?>
