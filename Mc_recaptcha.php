8fc95d007d3a8aabba9fd6318bf04903c344694f
onwhim <36354709+onwhim@users.noreply.github.com>
2018-04-30 11:09:48 +0800

    Made HTTP request POST rather than GET
    Change HTTP request type.

--- Mc_recaptcha.php
+++ Mc_recaptcha.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mc_recaptcha {
 	protected $CI = NULL;
-	protected $secret_key = "6LejJlYUAAAAAIZAlVFhH32MDY7-ma8fx3HZm_5P"; // Set your key here and you're ready to go...
-	protected $site_key = "6LejJlYUAAAAABTB3AZchm0wCqZJu3rya46kyPA-"; // Set your key here and you're ready to go...
+	protected $secret_key = "6LejJlYUADFDFFFlVFhH32MDY7-ma8fx3HZm_5P"; // Set your secret key here..
+	protected $site_key = "6LejJlYUAAAAAASDFDFGJWm0wCqZJu3rya46kyPA-"; // Set your site key here...
 
 	function __construct(){
 		$this->CI =& get_instance();
@@ -29,30 +29,47 @@ class Mc_recaptcha {
 	public function validated(){
 		$grr_post = $this->CI->input->post('g-recaptcha-response');
 		$user_ip = $this->CI->input->ip_address();
-		$output = $this->http_get("https://www.google.com/recaptcha/api/siteverify?secret=".$this->get_secret_key()."&response={$grr_post}&remoteip=".$user_ip);
+		$output = $this->http_post("https://www.google.com/recaptcha/api/siteverify?secret=".$this->get_secret_key()."&response={$grr_post}&remoteip=".$user_ip);
 		$output = json_decode($output, TRUE);
 		
 		if ($output['success']=='1') {
 			return TRUE;
 		}
-		
 		return FALSE;
 	}
 	
-	// HTTP Helper function
-	public function http_get($url){
-		$output = @file_get_contents($url);
+	// HTTP Helper Class
+	public function http_post($url){
+		$postdata = array();
+		
+		if(strpos($url,"?") !== FALSE){
+			$query_string_array = array();
+			$url_array = explode("?", $url);
+			$url = $url_array[0];
+			parse_str($url_array[1], $query_string_array);
+			$postdata = http_build_query($query_string_array);
+		}
+		
+		$opts = array('http' =>
+			array(
+				'method'  => 'POST',
+				'header'  => 'Content-type: application/x-www-form-urlencoded',
+				'content' => $postdata
+			)
+		);
 
+		$context  = stream_context_create($opts);
+		$output = @file_get_contents($url, false, $context);
+	
 		if($output === FALSE) {
 			if (function_exists('curl_init')){ 
-				$curl = curl_init();
-				curl_setopt($curl, CURLOPT_URL, $url);
-				curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-				curl_setopt($curl, CURLOPT_TIMEOUT, 15);
-				curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, TRUE);
-				curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2); 
-				$output = curl_exec($curl);
-				curl_close($curl);
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_POST, 1);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				$output = curl_exec($ch);
+				curl_close ($ch);
 			}
 		}
 		
